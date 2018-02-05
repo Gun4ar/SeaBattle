05692621c5f6f82d19044baf53c6a4ef5a9a05b6
Ilya Bogachev <32776363+Bogachev-Ilya@users.noreply.github.com>
2018-02-05 19:45:20 +0300

    Update Strategy.java

--- SeatBattle/src/Strategy.java
+++ SeatBattle/src/Strategy.java
@@ -1,25 +1,23 @@
 /**
- * Интерфейс Стратегий
+ * Класс Стратегий
  *
  * @author Илья Богачев
- * @since 28.01.2018
+ * @since 05.02.2018
  */
-public interface Strategy {
+public class Strategy {
 
-   default boolean fight(){
+   public boolean fight(){
        System.out.println("Default Player Fight method");
        return false;
    }
 
-   default boolean findShip(){
+   public boolean findShip(){
        System.out.println("Default Computer FindShips method");
        return false;
    }
 
-    default Point killWoundedShip(Point point){
+    public Point killWoundedShip(Point point){
         System.out.println("Default kill wounded ship");
         return point;
     }
-
-
 }
